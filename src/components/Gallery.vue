ffe294381778ffc22205a23442cd5f584697a8fd
Yiying Jie <yiying.j@outlook.com>
2021-06-01 21:24:35 -0700

    grid styling for images

---
+++ src/components/Gallery.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="gallery">
+    <div class="gallery-panel" v-for="card in cards" :key="card.id">
+      <img :src="thumbUrl(card.filename)">
+    </div>
+  </div>
+</template>
+
+<script>
+import cards from '@/cards.json';
+
+export default {
+  name: 'Gallery',
+  data() {
+    return {
+      cards,
+    };
+  },
+  methods: {
+    thumbUrl(filename) {
+      return require(`../../public/assets/images/thumbnails/${filename}`);
+    },
+  },
+}
+</script>
+
+<style scoped>
+.gallery {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
+  grid-gap: 1rem;
+  max-width: 80rem;
+  margin: 5rem auto;
+  padding: 0 5rem;
+}
+
+.gallery-panel img {
+  width: 100%;
+  height: 100%;
+  /* height: 22vw; */
+  object-fit: cover;
+  border-radius: 0.75rem;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <gallery />
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Gallery from '@/components/Gallery.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
-}
+    Gallery,
+  },
+};
 </script>
